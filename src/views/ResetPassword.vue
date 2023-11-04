b67ad3185963aea49ea0eb8f32cd69719246edd7
isai_ <isai_@ISAIREYES>
2023-11-03 18:00:06 -0600

    reset changes

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,53 +1,58 @@
 <template>
   <ion-page>
-    <ion-content :fullscreen="true">
-      <ion-item>
-        <ion-label class="float-label" position="floating"> Nombre </ion-label>
-        <ion-input v-model="form.nombre" id="nombre" required></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label class="float-label" position="floating">
-          Contraseña
-        </ion-label>
-        <ion-input v-model="form.password" id="password" required></ion-input>
-      </ion-item>
-      <ion-button @click="test">Default</ion-button>
+    <ion-content class="ion-padding">
+      <div class="imagen1"></div>
+      <div class="imagen2"></div>
+      <div class="imagen3"></div>
+      <v-img src="../assets/images/logotipo.svg"></v-img>
+      <v-label class="ma-4 text-h5 nunito-bold">Acceso al Sistema</v-label>
+      <v-text-field class="nunito-bold ion-margin tablet-input" label="RFC" rounded="lg" variant="solo"
+        v-model="form.email"></v-text-field>
+      <v-text-field class="nunito-bold ion-margin tablet-input" label="Contraseña" rounded="lg" variant="solo"
+        v-model="form.password" type="password"></v-text-field>
+      <ion-button @click="login" class="nunito-bold ion-margin" expand="full" color="danger">ENTRAR</ion-button>
 
-      <ion-button @click="resetPassword">Recuperar contraseña</ion-button>
-      
-      <ion-alert
-        :is-open="isOpen"
-        header="Alert"
-        sub-header="Important message"
-        message="This is an alert!"
-      ></ion-alert>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonContent, IonAlert } from "@ionic/vue";
-import { defineComponent } from "vue";
-import Swal from "sweetalert2";
+import { defineComponent, ref } from 'vue';
+import { IonPage, IonContent, IonButton } from '@ionic/vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 
 export default defineComponent({
-  data: () => ({
-    form: {
-      nombre: "",
-      password: "",
-    },
-    isOpen: false,
-  }),
-  methods: {
-    test() {
-      Swal.fire("SweetAlert2 is working!");
-    },
-    setOpen() {
-      this.isOpen = true;
-    },
-    resetPassword() {
-      return this.$router.push({ name: "resetPassword" });
-    },
+  setup() {
+    const router = useRouter();
+    const store = useStore();
+    const form = ref({
+      email: '',
+      password: '',
+    });
+
+    const login = async () => {
+      if (form.value.email === 'SOTJ841111Q39' && form.value.password === 'temporal') {
+        // Aquí puedes despachar la acción de inicio de sesión en Vuex si es necesario
+        // await store.dispatch('login', form.value);
+
+        // Navegar a la página de inicio de sesión exitosa o a donde desees
+        router.push({ name: 'dashboard' });
+      } else {
+        // Muestra una alerta de error en el inicio de sesión
+        // Puedes usar la función de alerta de Ionic o alguna otra personalización
+        console.log('Usuario y/o contraseña no válidos');
+      }
+    };
+
+    return {
+      form,
+      login,
+    };
   },
 });
 </script>
+
+<style>
+@import "../assets/css/reset.css";
+</style>
