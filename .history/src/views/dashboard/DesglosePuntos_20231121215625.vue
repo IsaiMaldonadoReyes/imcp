73e5fc262842e7f62d5c3850d8fc274a0c589019
Dulce Diaz <2516160077ddiazs@gmail.com>
2023-11-22 19:15:04 -0600

    fix desglose de puntos

---
+++ .history/src/views/dashboard/DesglosePuntos_20231121215625.vue
@@ -0,0 +1,21 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <v-container> </v-container>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data: () => ({
+    form: {
+      rfc: "",
+      password: "",
+    },
+  }),
+});
+</script>
---
+++ .history/src/views/dashboard/DesglosePuntos_20231121221050.vue
@@ -0,0 +1,26 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <v-container>Desgloce de puntos</v-container>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonContent } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "desglosePuntos",
+  components: {
+    IonContent,
+    IonPage,
+  },
+  data: () => ({
+    form: {
+      rfc: "",
+      password: "",
+    },
+  }),
+});
+</script>
